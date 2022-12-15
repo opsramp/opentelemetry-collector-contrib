457b88839ee079d6ac9f20ddbdd377e0a2a0e8e3
lokesh.balla <lokesh.balla@opsramp.com>
2022-12-15 17:03:10 +0530

    dropping requests if no resourceLogs in OpsRampOLTPExporter

--- exporter/opsrampotlpexporter/otlp.go
+++ exporter/opsrampotlpexporter/otlp.go
@@ -177,6 +177,9 @@ func (e *exporter) pushLogs(ctx context.Context, ld plog.Logs) error {
 	if e.config.ExpirationSkip != 0 {
 		e.skipExpired(ld)
 	}
+	if ld.ResourceLogs().Len() <= 0 {
+		return nil
+	}
 
 	req := plogotlp.NewRequestFromLogs(ld)
 
