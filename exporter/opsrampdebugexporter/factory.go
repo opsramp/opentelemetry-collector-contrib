ef14913ffcde1177d306b9b569c06de0e94896f6
Durgababu Neelam <durgababu.neelam@opsramp.com>
2024-05-18 18:37:35 +0530

    opsrampdebug exporter compilation issue fixes

--- exporter/opsrampdebugexporter/factory.go
+++ exporter/opsrampdebugexporter/factory.go
@@ -5,18 +5,20 @@ package opsrampdebugexporter // import "go.opentelemetry.io/collector/exporter/d
 
 import (
 	"context"
+	"errors"
+	"os"
+	"syscall"
 	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/opsrampdebugexporter/internal/metadata"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configtelemetry"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/exporter"
-	"go.opentelemetry.io/collector/exporter/debugexporter/internal/metadata"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
-	"go.opentelemetry.io/collector/exporter/internal/otlptext"
 )
 
 // The value of "type" key in configuration.
@@ -54,7 +56,7 @@ func createTracesExporter(ctx context.Context, set exporter.CreateSettings, conf
 		debugExporter.pushTraces,
 		exporterhelper.WithCapabilities(consumer.Capabilities{MutatesData: false}),
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
-		exporterhelper.WithShutdown(otlptext.LoggerSync(exporterLogger)),
+		exporterhelper.WithShutdown(loggerSync(exporterLogger)),
 	)
 }
 
@@ -66,7 +68,7 @@ func createMetricsExporter(ctx context.Context, set exporter.CreateSettings, con
 		debugExporter.pushMetrics,
 		exporterhelper.WithCapabilities(consumer.Capabilities{MutatesData: false}),
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
-		exporterhelper.WithShutdown(otlptext.LoggerSync(exporterLogger)),
+		exporterhelper.WithShutdown(loggerSync(exporterLogger)),
 	)
 }
 
@@ -78,7 +80,7 @@ func createLogsExporter(ctx context.Context, set exporter.CreateSettings, config
 		debugExporter.pushLogs,
 		exporterhelper.WithCapabilities(consumer.Capabilities{MutatesData: false}),
 		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
-		exporterhelper.WithShutdown(otlptext.LoggerSync(exporterLogger)),
+		exporterhelper.WithShutdown(loggerSync(exporterLogger)),
 	)
 }
 
@@ -91,4 +93,43 @@ func createLogger(cfg *Config, logger *zap.Logger) *zap.Logger {
 	)
 
 	return zap.New(core)
+}
+
+func loggerSync(logger *zap.Logger) func(context.Context) error {
+	return func(context.Context) error {
+		// Currently Sync() return a different error depending on the OS.
+		// Since these are not actionable ignore them.
+		err := logger.Sync()
+		osErr := &os.PathError{}
+		if errors.As(err, &osErr) {
+			wrappedErr := osErr.Unwrap()
+			if knownSyncError(wrappedErr) {
+				err = nil
+			}
+		}
+		return err
+	}
+}
+
+var knownSyncErrors = []error{
+	// sync /dev/stdout: invalid argument
+	syscall.EINVAL,
+	// sync /dev/stdout: not supported
+	syscall.ENOTSUP,
+	// sync /dev/stdout: inappropriate ioctl for device
+	syscall.ENOTTY,
+	// sync /dev/stdout: bad file descriptor
+	syscall.EBADF,
+}
+
+// knownSyncError returns true if the given error is one of the known
+// non-actionable errors returned by Sync on Linux and macOS.
+func knownSyncError(err error) bool {
+	for _, syncError := range knownSyncErrors {
+		if errors.Is(err, syncError) {
+			return true
+		}
+	}
+
+	return false
 }
