5f5e8328f533c2267aa67b0cedd7d4f560a0980e
mithunbelur <mithun-sateesh-babu.belur@hpe.com>
2024-05-29 16:52:39 +0530

    add logic to remove duplicate events of a resource

--- processor/opsrampk8sobjectsprocessor/log.go
+++ processor/opsrampk8sobjectsprocessor/log.go
@@ -5,47 +5,82 @@ package opsrampk8sobjectsprocessor // import "github.com/open-telemetry/opentele
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.uber.org/zap"
 )
 
-type logAttributesProcessor struct {
+type opsrampK8sObjectsProcessor struct {
 	logger *zap.Logger
 }
 
 // newOpsrampK8sObjectsProcessor returns a processor
-func newOpsrampK8sObjectsProcessor(logger *zap.Logger) *logAttributesProcessor {
-	return &logAttributesProcessor{
+func newOpsrampK8sObjectsProcessor(logger *zap.Logger) *opsrampK8sObjectsProcessor {
+	return &opsrampK8sObjectsProcessor{
 		logger: logger,
 	}
 }
 
-func (a *logAttributesProcessor) processLogs(ctx context.Context, ld plog.Logs) (plog.Logs, error) {
-	/*rls := ld.ResourceLogs()
+func (a *opsrampK8sObjectsProcessor) processLogs(ctx context.Context, ld plog.Logs) (plog.Logs, error) {
+	out := make(map[string]interface{})
+	rls := ld.ResourceLogs()
 	for i := 0; i < rls.Len(); i++ {
 		rs := rls.At(i)
 		ilss := rs.ScopeLogs()
-		resource := rs.Resource()
+		//resource := rs.Resource()
 		for j := 0; j < ilss.Len(); j++ {
 			ils := ilss.At(j)
 			logs := ils.LogRecords()
-			library := ils.Scope()
+			//library := ils.Scope()
 			for k := 0; k < logs.Len(); k++ {
 				lr := logs.At(k)
-				if a.skipExpr != nil {
-					skip, err := a.skipExpr.Eval(ctx, ottllog.NewTransformContext(lr, library, resource))
-					if err != nil {
-						return ld, err
-					}
-					if skip {
-						continue
-					}
+
+				var watchevent struct {
+					Object struct {
+						Metadata struct {
+							Uid string `json:"uid"`
+						} `json:"metadata"`
+					} `json:"object"`
+					Type string `json:"type"`
 				}
 
-				a.attrProc.Process(ctx, a.logger, lr.Attributes())
+				err := json.Unmarshal([]byte(lr.Body().AsString()), &watchevent)
+				if err != nil {
+					fmt.Println("Error unmarshalling JSON:", err)
+				}
+
+				out[watchevent.Object.Metadata.Uid] = lr
+
+				fmt.Printf("After unmarshal opsramp resourceLog #%d, scopeLog #%d, logRecord #%d UID %s \n", i, j, k, watchevent.Object.Metadata.Uid)
+
 			}
 		}
-	}*/
-	return ld, nil
+	}
+
+	/*
+		fmt.Println("Distinct event size\n", len(out))
+		for k, v := range out {
+			fmt.Println("Hi")
+			lr := v.(plog.LogRecord)
+			fmt.Printf("UID : %s, Data : %v\n", k, lr.Body().AsString())
+		}
+		// Sleep required because some times the print logs are not seen. Mosly processor is died before it prints/flushes.
+		time.Sleep(1 * time.Second)
+	*/
+
+	outld := plog.NewLogs()
+	resourceLogs := outld.ResourceLogs()
+
+	for _, v := range out {
+		rl := resourceLogs.AppendEmpty()
+		sl := rl.ScopeLogs().AppendEmpty()
+		logSlice := sl.LogRecords()
+		dstLogRecord := logSlice.AppendEmpty()
+		srcLogRecord := v.(plog.LogRecord)
+		srcLogRecord.CopyTo(dstLogRecord)
+	}
+
+	return outld, nil
 }
