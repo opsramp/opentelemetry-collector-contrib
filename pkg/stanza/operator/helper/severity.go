82360e87d1603c11711b82d9f453143f1c262a64
Suresh Kumar <60772288+pvrs2006sgnkl@users.noreply.github.com>
2026-03-01 18:49:00 +0530

    Merge/21.0.0 changes with 141
    carried the recent changes and required reasonable efforts to convert the opsramp agent into support 141 version.

--- pkg/stanza/operator/helper/severity.go
+++ pkg/stanza/operator/helper/severity.go
@@ -15,7 +15,7 @@ import (
 // SeverityParser is a helper that parses severity onto an entry.
 type SeverityParser struct {
 	ParseFrom     entry.Field
-	Mapping       severityMap
+	Mapping       SeverityMap
 	overwriteText bool
 }
 
@@ -43,13 +43,13 @@ func (p *SeverityParser) Parse(ent *entry.Entry) error {
 	return nil
 }
 
-type severityMap map[string]entry.Severity
+type SeverityMap map[string]entry.Severity
 
 // accepts various stringifyable input types and returns
 //  1. severity level if found, or default level
 //  2. string version of input value
 //  3. error if invalid input type
-func (m severityMap) find(value any) (entry.Severity, string, error) {
+func (m SeverityMap) find(value any) (entry.Severity, string, error) {
 	switch v := value.(type) {
 	case int:
 		strV := strconv.Itoa(v)
